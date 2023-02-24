de8ccc7656aab713191795ac09ad14784a10ff5d
Lucas Ertel <54690363+ertelsoares@users.noreply.github.com>
2023-02-23 21:38:45 -0300

    Git add blog
    Blog adicionado

--- database/seeders/ComentSeeder.php
+++ database/seeders/ComentSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Coment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class ComentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::factory(100)->create();
+        Coment::factory(100)->create();
+        
     }
 }
