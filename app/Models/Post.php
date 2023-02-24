7dc40f58acdb822866cffe718f3c2d95fb45edbd
Lucas Ertel <54690363+ertelsoares@users.noreply.github.com>
2023-02-24 20:45:10 -0300

    Adição e teste dos relacionamentos
    Teste dos relacionamentos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // pertence a um usuario
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+     //Essa função hasMany tem muitos
+    public function comments(){
+        return $this->hasMany(Coment::class);
+    }
 }
